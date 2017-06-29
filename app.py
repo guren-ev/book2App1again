62e7e200f8fba82fc80575625b969fd3cfe071b6
gabriel colon <gabriel.colon1@gmail.com>
2017-06-28 18:02:44 -0700

    added dynamic routes and Debug mode

--- app.py
+++ app.py
@@ -6,19 +6,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# error handeling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # to link the view function to a url
 @app.route("/")
 @app.route("/hello")
 
-#dynamic route
-@app.route("/test")
-def search():
-    return "helle this is search"
-
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "!!!!!!!!!!!!!!!!!!!!!!!!!1Hello, World!"
+
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+#RESPONSIVE OBJECTS
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "welebi":
+        return "Hello {}".format(name)
+    else:
+        return "Not Found", 404
+
+
 
 # start the develpmetn server using the run() method
 if __name__ == "__main__":
